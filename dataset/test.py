eda47a1d2877c5918a80dd7a80cc4e9d5e6f7c8f
Kaushal Gupta <kaushalkrgupta02@gmail.com>
2026-02-02 03:14:51 +0530

    fix: readme and added screenshots of dashboard

--- dataset/test.py
+++ dataset/test.py
@@ -2,9 +2,9 @@ import pandas as pd
 
 df = pd.read_csv('orders.csv')
 #replacing the old date year
-df['order_date'] = df['order_date'].str.replace('2015', '2026')
+df['order_date'] = df['order_date'].str.replace('2015', '2025')
 
-df.to_csv('orders_2026.csv', index=False)
+df.to_csv('orders_2025.csv', index=False)
 
 print("done year updation")
 #delete the orders.csv then
@@ -16,6 +16,6 @@ df = pd.read_csv('pizzas.csv')
 df['price'] = df['price'] - 10  # Normalizing prices for Indian market by subtracting  by 10
 df['price'] = df['price'] * 91  # Adjusting prices for inflation by multiplying by INR USD exchange rate
 df['price'] = df['price'].round(2) # upto two decimal places
-df.to_csv('pizzas_2026.csv', index=False)
+df.to_csv('pizzas_info_2025.csv', index=False)
 print("done price updation")
 #delete the pizzas.csv then
